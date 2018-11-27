424ab6d9eefc7e8c3eb0971d3c8cd085615e2e07
vlescot <vincent.lescot@gmail.com>
2018-11-27 16:56:51 +0100

    PSR Respect :close #7

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -3,7 +3,6 @@ declare(strict_types=1);
 
 namespace App\Repository;
 
-use App\Entity\Task;
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Common\Persistence\ManagerRegistry;
